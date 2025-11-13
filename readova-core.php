437bfb9168680f83e5e181b4781f470da2b354e5
Dhia Nabli <bytejutsuproject@gmail.com>
2025-11-13 11:52:03 +0100

    removed add line comment in the readova-core.php

--- readova-core.php
+++ readova-core.php
@@ -23,5 +23,4 @@ require_once plugin_dir_path(__FILE__) . 'inc/cpt-chapters.php';
 require_once plugin_dir_path(__FILE__) . 'inc/demo-chapters.php';
 require_once plugin_dir_path(__FILE__) . 'inc/editor/classic-editor.php';
 
-// ADD THIS LINE
 register_activation_hook(__FILE__, 'readova_core_activate_plugin');
