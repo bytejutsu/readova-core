855dde3beaa572b97cb1b22f2b62028e55a21e92
Dhia Nabli <bytejutsuproject@gmail.com>
2026-01-25 11:56:54 +0100

    updated the text domain name to match the expected one from readova to readova-core

--- readova-core.php
+++ readova-core.php
@@ -6,7 +6,7 @@
  * Author: ByteJutsu
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: readova
+ * Text Domain: readova-core
  */
 
 if (!defined('ABSPATH')) {
