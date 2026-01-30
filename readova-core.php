9ecdf7ca4a045dc7cb97174ac46dc06acc0ca226
Dhia Nabli <bytejutsuproject@gmail.com>
2026-01-30 14:07:47 +0100

    upgraded the version to 1.0.1

--- readova-core.php
+++ readova-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Readova Core
  * Description: Core functionality plugin for the Readova theme. Registers the 'Readova Chapters' custom post type and enables demo content import.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: ByteJutsu
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
